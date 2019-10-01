afa615c3405f4ddc150f963221f4827f8e12c360
Gabriel Carvalho <fale@gcarvalho.dev>
2019-09-30 21:05:39 -0300

    Pós formatação

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '724e6772a25b5fb688fe3beb51ae67cb',
+  expiresIn: '7d',
+};
---
+++ src/config/mail.js
@@ -0,0 +1,22 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4f7cadd1c6b126',
+    pass: '25424c8d385522',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gcarvalho.dev>',
+  },
+};
+
+/**
+ * Utilizar um dos seguintes emails
+ * Amazon SES //Utilizado pela rocketseat
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * GMail
+ *
+ * Mailtrap (DEV)
+ */
