9e8c2f5da0dd12be7477964e59841e9199e709af
Gabriel Carvalho <fale@gcarvalho.dev>
2019-11-16 01:07:43 -0300

    Fixes: removed debug line and updated yarn packages

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,6 +16,7 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
+    console.log(req.userId);
   } catch (err) {
     return res.status(401).json({ error: 'Token invalid' });
   }
